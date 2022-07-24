d5b86c43e08d819f94c02e2bd4d361f5f80fb480
Nanda Prabhu B <75189847+Nanda-prabhu@users.noreply.github.com>
2022-07-24 19:37:08 +0530

    add: Output

--- menuBasedCalculator.sh
+++ menuBasedCalculator.sh
@@ -5,8 +5,16 @@ read b
 echo -n "Enter operation: "
 read c
 case $c in
-"+") echo "$a + $b = " `expr $a + $b`;;
-"-") echo "$a - $b = " `expr $a - $b`;;
-"*") echo "$a * $b = " `expr $a * $b`;;
-"/") echo "$a / $b = " `expr $a / $b`;;
+  "+") echo "$a + $b = " `expr $a + $b`;;
+  "-") echo "$a - $b = " `expr $a - $b`;;
+  "*") echo "$a * $b = " `expr $a * $b`;;
+  "/") echo "$a / $b = " `expr $a / $b`;;
 esac
+
+# $ sh menuBasedCalculator.sh
+# Menu based Calculator
+# Enter Numbers:
+# 5
+# 5
+# Enter operation: +
+# 5 + 5 = 10
